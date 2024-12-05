05d68487894d22260d6045cdad7f9298c60d5ae0
Stefano Tambellini <17550149+mangowh@users.noreply.github.com>
2024-12-05 13:25:03 +0100

    day 4 part 2

--- include/day4.h
+++ include/day4.h
@@ -97,13 +97,59 @@ int searchXMAS(const std::vector<std::vector<char>> &matrix) {
 
   return count;
 }
+
+bool checkX_pattern(const std::vector<std::vector<char>> &matrix, int ax,
+                    int ay, std::string pattern) {
+  // nw M
+  bool nw = (ax > 0 && ay > 0 && matrix[ay - 1][ax - 1] == pattern[0]);
+
+  // ne S
+  bool ne = (ax < (matrix.size() - 1) && ay > 0 &&
+             matrix[ay - 1][ax + 1] == pattern[1]);
+
+  // sw M
+  bool sw = (ax > 0 && ay < (matrix[ax].size() - 1) &&
+             matrix[ay + 1][ax - 1] == pattern[2]);
+
+  // se S
+  bool se = (ax < (matrix.size() - 1) && ay < (matrix[ax].size() - 1) &&
+             matrix[ay + 1][ax + 1] == pattern[3]);
+
+  if (nw && ne && sw && se) {
+    return true;
+  }
+
+  return false;
+}
+
+int searchX_MAS(const std::vector<std::vector<char>> &matrix) {
+  int count{0};
+
+  for (int i = 0; i < matrix.size(); i++) {
+    auto &row = matrix[i];
+
+    for (int j = 0; j < row.size(); j++) {
+      auto &c = row[j];
+
+      if (c == 'A') {
+        if (detail::checkX_pattern(matrix, j, i, "MSMS") ||
+            detail::checkX_pattern(matrix, j, i, "MMSS") ||
+            detail::checkX_pattern(matrix, j, i, "SSMM") ||
+            detail::checkX_pattern(matrix, j, i, "SMSM")) {
+          count++;
+        }
+      }
+    }
+  }
+
+  return count;
+}
 } // namespace detail
 
 inline void part1() {
   std::cout << "day4::part1" << std::endl;
 
   std::ifstream fileStream{"./data/day4/input.txt"};
-  // std::ifstream fileStream{"./data/day4/small_input.txt"};
 
   if (!fileStream) {
     throw "Error trying to read input file";
@@ -131,5 +177,34 @@ inline void part1() {
   std::cout << foundXMAS << std::endl;
 }
 
-inline void part2() { std::cout << "day$::part2" << std::endl; }
+inline void part2() {
+  std::cout << "day4::part2" << std::endl;
+
+  std::ifstream fileStream{"./data/day4/input.txt"};
+
+  if (!fileStream) {
+    throw "Error trying to read input file";
+  }
+
+  // matrix[Y][X]
+  std::vector<std::vector<char>> data{};
+
+  std::string line;
+  int currentLineIdx = 0;
+  while (std::getline(fileStream, line)) {
+    std::vector<char> v{};
+
+    for (char &c : line) {
+      v.push_back(c);
+    }
+
+    data.push_back(v);
+
+    currentLineIdx++;
+  }
+
+  const int foundX_MAS{detail::searchX_MAS(data)};
+
+  std::cout << foundX_MAS << std::endl;
+}
 } // namespace day4
