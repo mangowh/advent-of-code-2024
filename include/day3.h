c6484a59d4a8e482dcc3e34924eccd037b1512eb
Stefano Tambellini <17550149+mangowh@users.noreply.github.com>
2024-12-05 10:18:33 +0100

    day3 part 2

--- include/day3.h
+++ include/day3.h
@@ -30,7 +30,46 @@ inline void part1() {
   }
 
   std::cout << sum << std::endl;
-}   
+}
 
-inline void part2() { std::cout << "day3::part2" << std::endl; }
+inline void part2() {
+  std::cout << "day3::part2" << std::endl;
+
+  std::ifstream t("./data/day3/input.txt");
+  std::stringstream buffer;
+  buffer << t.rdbuf();
+
+  std::regex mulExp("mul\\(([0-9]*),([0-9]*)\\)");
+  std::regex doExp("do\\(\\)");
+  std::regex dontExp("don't\\(\\)");
+
+  std::smatch match;
+
+  int sum{0};
+
+  bool enabled{true};
+  std::string token{};
+  while (std::getline(buffer, token, ')')) {
+    token += ')';
+
+    if (enabled) {
+      if (std::regex_search(token, match, mulExp)) {
+        int m1 = std::stoi(match[1]);
+        int m2 = std::stoi(match[2]);
+
+        sum += m1 * m2;
+      }
+    }
+
+    if (std::regex_search(token, doExp)) {
+      enabled = true;
+    }
+
+    if (std::regex_search(token, dontExp)) {
+      enabled = false;
+    }
+  }
+
+  std::cout << sum << std::endl;
+}
 } // namespace day3
