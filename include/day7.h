3692b0b9d6f5e239b7b81004672debc64a6b961d
Stefano Tambellini <17550149+mangowh@users.noreply.github.com>
2024-12-08 02:09:55 +0100

    day7 part2

--- include/day7.h
+++ include/day7.h
@@ -1,9 +1,73 @@
 #include <utils.h>
 
+#include <array>
 #include <fstream>
 #include <iostream>
+#include <list>
 #include <print>
+#include <ranges>
 #include <string>
+#include <tuple>
+#include <vector>
+
+// https://rosettacode.org/wiki/Permutations_with_repetitions#C++
+struct Generator {
+public:
+  Generator(int s, int v) : cSlots(s), cValues(v) {
+    a = new int[s];
+
+    for (int i = 0; i < cSlots - 1; i++) {
+      a[i] = 1;
+    }
+    a[cSlots - 1] = 0;
+
+    nextInd = cSlots;
+  }
+
+  ~Generator() { delete a; }
+
+  bool doNext() {
+    for (;;) {
+      if (a[nextInd - 1] == cValues) {
+        nextInd--;
+        if (nextInd == 0)
+          return false;
+      } else {
+        a[nextInd - 1]++;
+        while (nextInd < cSlots) {
+          nextInd++;
+          a[nextInd - 1] = 1;
+        }
+
+        return true;
+      }
+    }
+  }
+
+  void doPrint() {
+    printf("(");
+    for (int i = 0; i < cSlots; i++) {
+      printf("%d", a[i]);
+    }
+    printf(")");
+  }
+
+  std::vector<int> getCombination() {
+    std::vector<int> v{};
+
+    for (int i = 0; i < cSlots; i++) {
+      v.push_back(a[i]);
+    }
+
+    return v;
+  }
+
+private:
+  int *a;
+  int cSlots;
+  int cValues;
+  int nextInd;
+};
 
 namespace day7 {
 namespace detail {}
@@ -55,5 +119,60 @@ inline void part1() {
   std::cout << tot << std::endl;
 }
 
-inline void part2() { std::cout << "day7::part2" << std::endl; }
+inline void part2() {
+  std::cout << "day7::part2" << std::endl;
+
+  long long tot{0};
+
+  std::ifstream myfile("./data/day7/input.txt");
+  std::string line{};
+  while (std::getline(myfile, line)) {
+    const auto row = utils::splitStringByChar(line, ':');
+
+    const auto result = std::stoll(row[0]);
+
+    const auto valuesString = row[1].substr(1, row[1].size());
+    const auto values = utils::splitStringByChar(valuesString, ' ');
+
+    const int slots = static_cast<int>(values.size() - 1);
+
+    Generator g(slots, 3);
+
+    while (g.doNext()) {
+      auto combination = g.getCombination();
+
+      long long sum{std::stoll(values[0])};
+
+      for (int k = 0; k < combination.size(); k++) {
+        const auto nextValue = std::stoll(values[k + 1]);
+
+        switch (combination[k]) {
+        case (1): {
+          sum *= nextValue;
+          break;
+        }
+        case (2): {
+          sum += nextValue;
+          break;
+        }
+        case (3): {
+          const auto t = std::to_string(sum);
+          const auto n = std::to_string(nextValue);
+          const auto conc = t + n;
+
+          sum = stoll(conc);
+          break;
+        }
+        }
+      }
+
+      if (result == sum) {
+        tot += sum;
+        break;
+      }
+    }
+  }
+
+  std::cout << tot << std::endl;
+}
 } // namespace day7
--- main.cpp
+++ main.cpp
@@ -18,13 +18,12 @@ int main() {
     part2();
     auto t3 = high_resolution_clock::now();
 
-    /* Getting number of milliseconds as an integer. */
     auto part1Bench = duration_cast<milliseconds>(t2 - t1);
     auto part2Bench = duration_cast<milliseconds>(t3 - t2);
 
     std::cout << std::endl;
-    std::cout << "part1 took: " << part1Bench << " ms" << std::endl;
-    std::cout << "part2 took: " << part2Bench << " ms" << std::endl;
+    std::cout << "part1 took: " << part1Bench << std::endl;
+    std::cout << "part2 took: " << part2Bench << std::endl;
 
     return EXIT_SUCCESS;
   } catch (std::exception e) {
