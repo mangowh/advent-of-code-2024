0eeefdc29dc936bd8bf415ac16a113f7e7183981
Stefano Tambellini <17550149+mangowh@users.noreply.github.com>
2024-12-10 12:56:37 +0100

    day9 part1

---
+++ include/day9.h
@@ -0,0 +1,75 @@
+#include <fstream>
+#include <iostream>
+#include <print>
+#include <string>
+#include <vector>
+
+namespace day9 {
+namespace detail {}
+
+inline void part1() {
+  std::cout << "day9::part1" << std::endl;
+
+  std::fstream fileStream("./data/day9/input.txt");
+
+  std::vector<long long> data;
+
+  long count{0};
+  bool isFile{true};
+  std::string line;
+  std::getline(fileStream, line);
+
+  for (const char c : line) {
+    const int n = c - '0';
+
+    for (size_t k = 0; k < n; k++) {
+      if (isFile) {
+        data.push_back(count / 2);
+      } else {
+        data.push_back(-1); // '.'
+      }
+    }
+
+    count++;
+    isFile = !isFile;
+  }
+
+  // for (auto el : data) {
+  //   if (el == -1) {
+  //     std::cout << '.';
+  //   } else {
+  //     std::cout << el;
+  //   }
+  // }
+  // std::cout << std::endl;
+
+  for (size_t i = 0; i < data.size(); i++) {
+    if (data[i] == -1) {
+      for (size_t k = data.size() - 1; k >= 0; k--) {
+        if (i >= k) {
+          goto exit;
+        }
+
+        if (data[k] != -1) {
+          std::swap(data[i], data[k]);
+          break;
+        }
+      }
+    }
+  }
+
+exit:
+  long long sum{0};
+  for (int i = 0; i < data.size(); i++) {
+    if (data[i] == -1) {
+      break;
+    }
+
+    sum += data[i] * i;
+  }
+
+  std::cout << sum << std::endl;
+}
+
+inline void part2() { std::cout << "day9::part2" << std::endl; }
+} // namespace day9
--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-﻿#include "day8.h"
+﻿#include "day9.h"
 
 #include <chrono>
 #include <iostream>
 
-using namespace day8;
+using namespace day9;
 
 int main() {
   using std::chrono::duration;
@@ -15,11 +15,15 @@ int main() {
     auto t1 = high_resolution_clock::now();
     part1();
     auto t2 = high_resolution_clock::now();
-    part2();
+
+    std::cout << std::endl;
+
     auto t3 = high_resolution_clock::now();
+    part2();
+    auto t4 = high_resolution_clock::now();
 
     auto part1Bench = duration_cast<milliseconds>(t2 - t1);
-    auto part2Bench = duration_cast<milliseconds>(t3 - t2);
+    auto part2Bench = duration_cast<milliseconds>(t4 - t3);
 
     std::cout << std::endl;
     std::cout << "part1 took: " << part1Bench << std::endl;
