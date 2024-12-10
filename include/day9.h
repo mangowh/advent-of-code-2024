c9e8e9997ac193d7ea5d5fb2737ed13ce4b0192f
Stefano Tambellini <17550149+mangowh@users.noreply.github.com>
2024-12-10 13:56:38 +0100

    wip day9 part 2

--- include/day9.h
+++ include/day9.h
@@ -71,5 +71,102 @@ exit:
   std::cout << sum << std::endl;
 }
 
-inline void part2() { std::cout << "day9::part2" << std::endl; }
+inline void part2() {
+  std::cout << "day9::part2" << std::endl;
+  std::fstream fileStream("./data/day9/input.txt");
+
+  std::vector<long long> data;
+
+  long count{0};
+  bool isFile{true};
+  std::string line;
+  std::getline(fileStream, line);
+
+  for (const char c : line) {
+    const int n = c - '0';
+
+    for (size_t k = 0; k < n; k++) {
+      if (isFile) {
+        data.push_back(count / 2);
+      } else {
+        data.push_back(-1); // '.'
+      }
+    }
+
+    count++;
+    isFile = !isFile;
+  }
+
+  for (auto el : data) {
+    if (el == -1) {
+      std::cout << '.';
+    } else {
+      std::cout << el;
+    }
+  }
+  std::cout << std::endl;
+
+  int k = static_cast<int>(data.size()) - 1;
+  while (k >= 0) {
+    if (data[k] != -1) {
+      const auto val = data[k];
+
+      int valSize{1};
+      while (k - valSize >= 0 && data[k - valSize] == val) {
+        valSize++;
+      }
+
+      int i = 0;
+      while (i < data.size()) {
+        if (data[i] == -1) {
+          int spaceSize{1};
+          while (i + spaceSize < data.size() && data[i + spaceSize] == -1) {
+            spaceSize++;
+          }
+
+          if (valSize <= spaceSize) {
+            for (int spaceCursor = i; spaceCursor < i + valSize;
+                 spaceCursor++) {
+              data[spaceCursor] = val;
+            }
+
+            for (int valCursor = k; valCursor > k - valSize; valCursor--) {
+              data[valCursor] = -1;
+            }
+
+            for (auto el : data) {
+              if (el == -1) {
+                std::cout << '.';
+              } else {
+                std::cout << el;
+              }
+            }
+            std::cout << std::endl;
+
+            i += valSize;
+
+            break;
+          }
+        }
+
+        i++;
+      }
+
+      k -= valSize;
+    } else {
+      k--;
+    }
+  }
+
+  long long sum{0};
+  for (int i = 0; i < data.size(); i++) {
+    if (data[i] == -1) {
+      break;
+    }
+
+    sum += data[i] * i;
+  }
+
+  std::cout << sum << std::endl;
+}
 } // namespace day9
