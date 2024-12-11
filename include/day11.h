ce8bc9de36db9e1bf14f43e1eab8ed761f65da72
Stefano Tambellini <17550149+mangowh@users.noreply.github.com>
2024-12-11 16:52:45 +0100

    wip day11 part2

--- include/day11.h
+++ include/day11.h
@@ -4,6 +4,7 @@
 #include <print>
 #include <sstream>
 #include <string>
+#include <utility>
 #include <vector>
 
 namespace day11 {
@@ -69,7 +70,7 @@ inline void part2() {
 
   auto iss = std::istringstream{line};
 
-  int blinkNum = 25;
+  int blinkNum = 75;
   std::vector<long long> data;
 
   std::string stringValue;
@@ -77,35 +78,43 @@ inline void part2() {
     data.push_back(std::stoi(stringValue));
   }
 
+  std::mutex lock_;
+
   while (blinkNum--) {
+    std::vector<std::pair<size_t, long long>> toAdd;
+
+    std::cout << data.size() << std::endl;
+
     size_t count{0};
-
-    std::for_each(std::execution::par, data.begin(), data.end(),
-                  [&data, &count](auto &&item) {
-                    auto num = item;
-
+    std::for_each(std::execution::par_unseq, data.begin(), data.end(),
+                  [&toAdd, count, &lock_](long long &num) {
                     if (num == 0) {
                       num = 1;
+                    } else {
+                      std::string s = std::to_string(num);
+                      if (s.length() % 2 == 0) {
+                        std::string half = s.substr(0, s.length() / 2);
+                        std::string otherHalf = s.substr(s.length() / 2);
+
+                        long long firstNum = std::stoll(half);
+                        long long secondNum = std::stoll(otherHalf);
+
+                        std::lock_guard<std::mutex> lg(lock_);
+
+                        num = firstNum;
+                        toAdd.push_back(std::make_pair(count + 1, secondNum));
+                      } else {
+                        num *= 2024;
+                      }
                     }
-
-                    std::string s = std::to_string(num);
-
-                    if (s.length() % 2 == 0) {
-                      std::string half = s.substr(0, s.length() / 2);
-                      std::string otherHalf = s.substr(s.length() / 2);
-
-                      long long firstNum = std::stoll(half);
-                      long long secondNum = std::stoll(otherHalf);
-
-                      num = firstNum;
-                      data.insert(data.begin() + count + 1, secondNum);
-
-                      count++;
-                    }
-
-                    num *= 2024;
-                    count++;
                   });
+
+    for (const auto &[idx, value] : toAdd) {
+      // data.insert(data.begin() + idx + 1, value);
+      data.push_back(value);
+    }
   }
+
+  std::cout << data.size() << std::endl;
 }
 } // namespace day11
